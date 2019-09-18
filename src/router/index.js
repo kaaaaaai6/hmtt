abdf3a855ccc80ebb3bdb9f17c76b8bfb9bafc38
吴凯轩 <kaaaaaai6@163.com>
2019-09-18 19:41:00 +0800

    调整目录结构

---
+++ src/router/index.js
@@ -0,0 +1,13 @@
+// 引入组件
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+// 使用组件
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  // 配置路由表
+  routes: []
+})
+
+export default router
